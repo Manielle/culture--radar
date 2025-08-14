955720ffec69a2859ca070d8d34d53e9942ce140
Manielle <75130972+Manielle@users.noreply.github.com>
2025-08-14 15:22:17 +0200

    Update config.php

--- config.php
+++ config.php
@@ -7,7 +7,7 @@
 // Configure session settings ONLY if session hasn't started yet
 if (session_status() === PHP_SESSION_NONE) {
     ini_set('session.cookie_httponly', 1);
-    ini_set('session.cookie_secure', 1); //
+    ini_set('session.cookie_secure', 0); // Mettre à 1 en HTTPS uniquement
     ini_set('session.use_strict_mode', 1);
 }
 
